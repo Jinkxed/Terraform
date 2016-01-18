b21c8471137eedeea50033b3be4269394ec91096
Chad Upton <chad.upton@findly.com>
2016-01-18 08:32:30 -0600

    adding terraform bug

--- plans/example/vpc.tf
+++ plans/example/vpc.tf
@@ -12,6 +12,9 @@ module "vpc" {
 
   name            = "${lookup(var.vpc-setting, name)}"
   cidr            = "${lookup(var.vpc-setting, cidr)}"
+  # Bug seems to be here, these variables must be static like:
+  # private_subnets = "10.0.1.0/24,10.0.2.0/24,10.0.3.0/24"
+  # public_subnets = "10.0.101.0/24,10.0.102.0/24,10.0.103.0/24"
   private_subnets = "${lookup(var.vpc-setting, private_subnets)}"
   public_subnets  = "${lookup(var.vpc-setting, public_subnets)}"
   azs             = "${lookup(var.vpc-setting, azs)}"
